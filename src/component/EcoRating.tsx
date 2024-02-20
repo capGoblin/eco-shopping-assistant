d6e547af35dd465e583b8635ff2af7fe72872133
kamalesh083 <kamalesh083@gmail.com>
2024-02-20 16:06:19 +0530

    StarRating

---
+++ src/component/EcoRating.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+
+
+interface StarRatingProps {
+    totalStars: number;
+  }
+function EcoRating  ({totalStars}:StarRatingProps) {
+    
+        const [rating, setRating] = useState<number>(0);
+      
+        const handleClick = (starIndex: number) => {
+          if (starIndex === rating - 1) {
+            setRating(0);
+          } else {
+            setRating(starIndex + 1);
+          }
+        };
+ return (
+    <div>
+        {[...Array(totalStars)].map((_, index) => (
+        <span
+          key={index}
+          onClick={() => handleClick(index)}
+          style={{ cursor: 'pointer', color: index < rating ? 'gold' : 'gray' }}
+        >
+          &#9733;
+        </span>
+      ))}
+      <p>Current Rating: {rating}</p>
+      
+    </div>
+  );
+}
+
+export default EcoRating;
---
+++ src/component/ProductDate.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductDate = () => {
+  return <div>ProductDate</div>;
+};
+
+export default ProductDate;
